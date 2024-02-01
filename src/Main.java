53ae92e577cd92ea412a6cf2ab1161e3b9f7c57f
Roni <roniapollo1@gmail.com>
2024-02-01 19:09:03 +0200

    added scanner to get matrix from user(including letting user check if they entered it right) and gaussian elimination algorithm itself

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,81 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
+import java.util.Scanner;
 public class Main {
+    static int rows;
+    static int columns;
     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int[][] matrix=getMatrixFromUser(scan);
+
+        //make sure user entered matrix correctly
+        System.out.println("\nData you entered :");
+        printMatrix(matrix);
+        System.out.println("is this correct? Write true or false");
+        if(!scan.nextBoolean()){
+            System.out.println("Reenter matrix");
+            matrix=getMatrixFromUser(scan);
+        }
+
+        //start gaussian elimination itself
+        int rowIndex=0;
+        int columnIndex=0;
+        int rowWithoutPivot=0;
+        while(columnIndex<columns){
+            while(rowIndex<rows) {
+                if (matrix[rowIndex][columnIndex] == 0)
+                    rowIndex++;
+                else{
+                    for(int i=rowIndex+1;i<rows;i++){
+                        if(matrix[i][columnIndex]!=0) {
+                            int scalar=(-1*matrix[i][columnIndex])*(1/matrix[rowIndex][columnIndex]);
+                            addToRow(matrix, i, rowIndex, scalar);
+                        }
+                    }
+                    switchRows(matrix, rowIndex, rowWithoutPivot);
+                    rowWithoutPivot++;
+                }
+            }
+            rowIndex=0;
+            columnIndex++;
+        }
+
 
     }
-    protected int[][] addToRow(int[][] matrix, int addTo, int otherRow, int scalar){
-        for(int i = 0;i<matrix[addTo].length;i++)
-            matrix[addTo][i]+= scalar * matrix[otherRow][i];
+    protected static void printMatrix(int[][] matrix){
+        for(int[] x:matrix){
+            for(int y:x){
+                System.out.print(y+"        ");
+            }
+            System.out.print("\n");
+        }
+    }
+    protected static int[][] getMatrixFromUser(Scanner scan){
+        //make matrix based on user input
+        System.out.println("Enter 2D matrix size : ");
+        rows=scan.nextInt();
+        columns=scan.nextInt();
+
+        System.out.println("Enter elements of matrix : ");
+        int[][] matrix =new int[rows][columns];
+        for(int i=0; i<rows;i++)
+        {
+            for(int j=0; j<columns;j++)
+            {
+                matrix[i][j]=scan.nextInt();
+            }
+        }
         return matrix;
     }
-    protected int[][] switchRows(int[][] matrix, int rowOne, int rowTwo){
+    protected static void addToRow(int[][] matrix, int addTo, int otherRow, int scalar){
+        for(int i = 0;i<columns;i++)
+            matrix[addTo][i]+= scalar * matrix[otherRow][i];
+    }
+    protected static void switchRows(int[][] matrix, int rowOne, int rowTwo){
         int[] temp=matrix[rowOne].clone();
-        for(int i=0;i<matrix[rowOne].length;i++) {
+        for(int i=0;i<columns;i++) {
             matrix[rowOne][i] = matrix[rowTwo][i];
             matrix[rowTwo][i] = temp[i];
         }
-        return matrix;
     }
 }
